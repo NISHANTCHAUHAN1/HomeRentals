e1e2ef0e3eddedf9fcddbd8f4d67f32bbe2f3e52
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-09-04 13:39:25 -0400

    wishList

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,4 +1,6 @@
 import Booking from "../models/bookingModel.js";
+import Listing from "../models/listing.model.js";
+import User from "../models/userModel.js";
 import TryCatch from "../utils/TryCatch.js";
 
 export const getTripList = TryCatch(async(req,res) => {
@@ -9,3 +11,26 @@ export const getTripList = TryCatch(async(req,res) => {
 
     res.status(200).json(trips);
 })
+
+export const addListingInWishList = TryCatch(async(req,res) => {
+    const { userId, listingId } = req.params
+
+    const user = await User.findById(userId);
+    if(!user) return res.status(400).json({message: "User Not Found"});
+
+    const listing = await Listing.findById(listingId).populate("creator")
+    if(!listing) return res.status(400).json({message: "Listing Not Found"});
+
+    const favListing = user.wishList.find((item) => item._id.toString() === listingId);
+
+    if (favListing) {
+      user.wishList = user.wishList.filter((item) => item._id.toString() !== listingId)
+
+      await user.save()
+      res.status(200).json({message: "Removed From Wishlist",wishList: user.wishList,})
+    } else {
+      user.wishList.push(listing)
+      await user.save()
+      res.status(200).json({message: "Added To Wishlist",wishList: user.wishList,})
+    }
+});
--- Backend/routes/uesrListRoute.js
+++ Backend/routes/uesrListRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getTripList } from "../controller/userController.js";
+import { addListingInWishList, getTripList } from "../controller/userController.js";
 
 const router = express.Router();
 
-router.get("/:userId/trips", getTripList)
+router.get("/:userId/trips", getTripList);
+router.patch("/:userId/:listingId", addListingInWishList);
 
 export default router;
