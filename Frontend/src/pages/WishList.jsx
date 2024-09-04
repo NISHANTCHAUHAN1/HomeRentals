0fd64d6619d2c164ed9a16fbd765db6b0ae42fb0
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-09-04 18:58:47 -0400

    edit wishlist

--- Frontend/src/pages/WishList.jsx
+++ Frontend/src/pages/WishList.jsx
@@ -6,7 +6,7 @@ import ListingCard from "../components/ListingCard";
 const WishList = () => {
   const wishList = useSelector((store) => store?.user?.wishList);
   // console.log(wishList);
-  
+
   return (
     <>
       <Navbar />
@@ -16,32 +16,44 @@ const WishList = () => {
       </h1>
 
       <div className="px-24 pb-28 flex justify-center flex-wrap gap-6">
-        {wishList?.map(
-          ({
-            _id,
-            creator,
-            listingPhotoPaths,
-            city,
-            state,
-            country,
-            category,
-            type,
-            price,
-            booking = false,
-          }) => (
-            <ListingCard
-              listingId={_id}
-              creator={creator}
-              listingPhotoPaths={listingPhotoPaths}
-              city={city}
-              state={state}
-              country={country}
-              category={category}
-              type={type}
-              price={price}
-              booking={booking}
-            />
+        {wishList.length > 0 ? (
+          wishList?.map(
+            ({
+              _id,
+              creator,
+              listingPhotoPaths,
+              city,
+              state,
+              country,
+              category,
+              type,
+              price,
+              booking = false,
+            }) => (
+              <ListingCard
+                listingId={_id}
+                creator={creator}
+                listingPhotoPaths={listingPhotoPaths}
+                city={city}
+                state={state}
+                country={country}
+                category={category}
+                type={type}
+                price={price}
+                booking={booking}
+              />
+            )
           )
+        ) : (
+          <>
+            <div>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTcKcoquPvJ-O9WfgEYiUF34hYhzaGcrtamQ&s"
+                alt=""
+              />
+              <h1 className="text-2xl mt-10 text-center text-sky-600">Not Found WishList</h1>
+            </div>
+          </>
         )}
       </div>
     </>
