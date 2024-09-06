66dcb3eb8376e548ac82dd94cc1ffe77081f0e02
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-09-06 01:45:12 -0400

    footer edit

--- Frontend/src/pages/Footer.jsx
+++ Frontend/src/pages/Footer.jsx
@@ -4,9 +4,6 @@ function Footer() {
   return (
     <section className="w-full overflow-hidden py-8 bg-white text-textColor border-t border-gray-500">
       <div className="flex h-full justify-center items-center px-12 mx-auto">
-        {/* <div className="mb-4 inline-flex items-center">
-          logo
-        </div> */}
         <div>
           <p className="text-lg font-semibold">
             &copy; 2024{" "}
