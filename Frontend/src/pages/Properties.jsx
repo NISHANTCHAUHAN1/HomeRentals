40761152c6b49f47d8608e5c16746ff90ff68789
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-09-06 15:21:41 -0400

    minor change

--- Frontend/src/pages/Properties.jsx
+++ Frontend/src/pages/Properties.jsx
@@ -5,11 +5,94 @@ import { useDispatch, useSelector } from "react-redux";
 import { setPropertyList } from "../redux/userSlice";
 import ListingCard from "../components/ListingCard";
 
-const Properties = () => {
-  const user = useSelector((state) => state?.user?.user);
+// const Properties = () => {
+//   const user = useSelector((state) => state?.user?.user);
 
+//   const propertyList = useSelector((state) => state?.user?.propertyList);
+//   // console.log(propertyList)
+
+//   const dispatch = useDispatch();
+
+//   const getPropertyList = async () => {
+//     try {
+//       const response = await fetch(
+//         `http://localhost:3000/api/userlist/${user._id}/properties`,
+//         {
+//           method: "GET",
+//         }
+//       );
+//       const data = await response.json();
+//       //   console.log(data)
+//       dispatch(setPropertyList(data));
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     getPropertyList();
+//   }, []);
+//   return (
+//     <>
+//       <Navbar />
+
+//       <h1 className="text-2xl font-bold text-slate-700 my-10 mx-[100px] sm:mx-12">
+//         Your Property List
+//       </h1>
+
+//       <div className="px-24 pb-28 flex justify-center flex-wrap gap-6">
+//         {propertyList > 0 ? (
+//           propertyList?.map(
+//             ({
+//               _id,
+//               creator,
+//               listingPhotoPaths,
+//               city,
+//               state,
+//               country,
+//               category,
+//               type,
+//               price,
+//               booking = false,
+//             }) => (
+//               <ListingCard
+//                 listingId={_id}
+//                 creator={creator}
+//                 listingPhotoPaths={listingPhotoPaths}
+//                 city={city}
+//                 state={state}
+//                 country={country}
+//                 category={category}
+//                 type={type}
+//                 price={price}
+//                 booking={booking}
+//               />
+//             )
+//           )
+//         ) : (
+//           <div>
+//             <img
+//               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTcKcoquPvJ-O9WfgEYiUF34hYhzaGcrtamQ&s"
+//               alt=""
+//             />
+//             <h1 className="text-2xl mt-10 text-center text-sky-600">
+//               Not Found Property List
+//             </h1>
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
+
+const PropertyList = () => {
+  const user = useSelector((state) => state.user.user);
+
+  // const propertyList = user?.propertyList
   const propertyList = useSelector((state) => state?.user?.propertyList);
-  // console.log(propertyList)
+  //   // console.log(propertyList)
+
+  console.log(propertyList);
 
   const dispatch = useDispatch();
 
@@ -21,7 +104,9 @@ const Properties = () => {
           method: "GET",
         }
       );
+
       const data = await response.json();
+
       //   console.log(data)
       dispatch(setPropertyList(data));
     } catch (error) {
@@ -32,6 +117,7 @@ const Properties = () => {
   useEffect(() => {
     getPropertyList();
   }, []);
+
   return (
     <>
       <Navbar />
@@ -41,7 +127,7 @@ const Properties = () => {
       </h1>
 
       <div className="px-24 pb-28 flex justify-center flex-wrap gap-6">
-        {propertyList > 0 ? (
+        {propertyList.length > 0 ? (
           propertyList?.map(
             ({
               _id,
@@ -76,7 +162,7 @@ const Properties = () => {
               alt=""
             />
             <h1 className="text-2xl mt-10 text-center text-sky-600">
-              Not Found Property List
+              Not Found WishList
             </h1>
           </div>
         )}
@@ -84,5 +170,4 @@ const Properties = () => {
     </>
   );
 };
-
-export default Properties;
+export default PropertyList;
